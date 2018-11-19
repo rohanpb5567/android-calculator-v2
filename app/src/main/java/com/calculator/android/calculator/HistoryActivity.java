5e71ca17b5a757138631c55dd901008f8dedc0b2
rohanpb5567 <rohanpb5567@gmail.com>
2018-11-19 15:44:40 -0500

    Add HistoryObject.

--- app/src/main/java/com/calculator/android/calculator/HistoryActivity.java
+++ app/src/main/java/com/calculator/android/calculator/HistoryActivity.java
@@ -3,6 +3,8 @@ package com.calculator.android.calculator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,11 +12,24 @@ import android.widget.Toast;
 public class HistoryActivity extends AppCompatActivity {
 
     private Button mBack2Main;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        mRecyclerView = (RecyclerView) findViewById(R.id.history_recycler);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MyAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+
 
         mBack2Main = (Button) findViewById(R.id.button_back2main);
         mBack2Main.setOnClickListener(new View.OnClickListener() {
